d740449b75bb59210cfa3cd0763dcd129b5e0c10
Mike Harms <harmsm@gmail.com>
2016-05-23 10:15:47 -0700

    install now works as expected

--- PyCmdMessenger/__init__.py
+++ PyCmdMessenger/__init__.py
@@ -7,5 +7,4 @@ __author__ = "Michael J. Harms"
 __date__ = "2016-05-23"
 __all__ = ["PyCmdMessenger"]
 
-import PyCmdMessenger
-from PyCmdMessenger import PyCmdMessenger
+from .PyCmdMessenger import PyCmdMessenger as PyCmdMessenger
