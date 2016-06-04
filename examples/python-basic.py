28582fc1f08d3e0814410d89532ba8e192927f93
Mike Harms <harmsm@gmail.com>
2016-06-03 21:14:23 -0700

    updated test

--- examples/python-basic.py
+++ examples/python-basic.py
@@ -7,25 +7,24 @@ import PyCmdMessenger
 # Initialize an ArduinoBoard instance.  This is where you specify baud rate and
 # serial timeout.  If you are using a non ATmega328 board, you might also need
 # to set the data sizes (bytes for integers, longs, floats, and doubles).  
-a = PyCmdMessenger.ArduinoBoard("/dev/ttyACM0",baud_rate=9600)
+arduino = PyCmdMessenger.ArduinoBoard("/dev/ttyACM0",baud_rate=9600)
 
 # List of command_names in arduino file. These must be in the same order as in
 # the sketch.
-command_names = ["who_are_you","sum_two_ints","result","error"]
+command_names = ["who_are_you","my_name_is","sum_two_ints","sum_is","error"]
 
 # List of data types being sent/recieved for each command, again in the same 
 # order. 
-command_formats = ["","ii","i","s"]
+command_formats = ["","s","ii","i","s"]
 
 # Initialize the messenger
 c = PyCmdMessenger.CmdMessenger(arduino,
                                 command_names=command_names,
-                                command_formats=commmand_formats)
+                                command_formats=command_formats)
 
 # Send
 c.send("who_are_you")
-
-# Receive. Should give ["result","Bob",TIME_RECIEVED]
+# Receive. Should give ["my_name_is","Bob",TIME_RECIEVED]
 msg = c.receive()
 print(msg)
 
@@ -33,5 +32,5 @@ print(msg)
 c.send("sum_two_ints",4,1)
 msg = c.receive()
 
-# should give ["result",5,TIME_RECEIVED]
+# should give ["sum_is",5,TIME_RECEIVED]
 print(msg)
